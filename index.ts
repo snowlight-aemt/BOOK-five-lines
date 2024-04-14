a637ad08ee3f97308ce6d499673ad1861e41a069
snowlight-aemt <it.s.aemt@gmail.com>
2024-04-14 16:51:02 +0900

    refactor: if-else 2단계 클래스로 코드 이관

--- index.ts
+++ index.ts
@@ -15,35 +15,28 @@ enum Tile {
 }
 
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isDown(): boolean;
-  isUp(): boolean;
+  handle(): void;
 }
 
 class Right implements Input {
-  isDown(): boolean {return false;}
-  isLeft(): boolean {return false;}
-  isRight(): boolean {return true;}
-  isUp(): boolean {return false;}
-}
-class Up implements Input {
-  isDown(): boolean {return false;}
-  isLeft(): boolean {return false;}
-  isRight(): boolean {return false;}
-  isUp(): boolean {return true;}
+  handle() {
+    moveHorizontal(1);
+  }
 }
 class Left implements Input {
-  isDown(): boolean {return false;}
-  isLeft(): boolean {return true;}
-  isRight(): boolean {return false;}
-  isUp(): boolean {return false;}
+  handle() {
+    moveHorizontal(-1);
+  }
+}
+class Up implements Input {
+  handle() {
+    moveVertical(1);
+  }
 }
 class Down implements Input {
-  isDown(): boolean {return true;}
-  isLeft(): boolean {return false;}
-  isRight(): boolean {return false;}
-  isUp(): boolean {return false;}
+  handle() {
+    moveVertical(-1);
+  }
 }
 
 let playerx = 1;
@@ -121,14 +114,7 @@ function handleInputs() {
 }
 
 function handleInput(input: Input) {
-  if (input.isLeft())
-    moveHorizontal(-1);
-  else if (input.isRight())
-    moveHorizontal(1);
-  else if (input.isUp())
-    moveVertical(-1);
-  else if (input.isDown())
-    moveVertical(1);
+  input.handle()
 }
 
 function updateMap() {
